6ad063986315e8e0d139d5837704cf2583cef558
CoelhoTv76 <77584870+CoelhoTv76@users.noreply.github.com>
2021-05-13 12:37:18 -0300

    Update cekvip.js

--- src/cekvip.js
+++ src/cekvip.js
@@ -1,7 +1,7 @@
 const cekvip = () => { 
 	return `           
 ──────────────────
-*Nome do bot* :  CAUSS BOT
+*Nome do bot* :  DECIM BOT
 ──────────────────
         『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
 ──────────────────
