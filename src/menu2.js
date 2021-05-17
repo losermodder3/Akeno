37fcfe9f6456a6240a2848b251524a8429064cda
losermodder3 <84289562+losermodder3@users.noreply.github.com>
2021-05-17 12:46:23 -0300

    Update menu2.js

--- src/menu2.js
+++ src/menu2.js
@@ -2,48 +2,48 @@ const menu2 = (prefix) => {
 	return `                 
 ╠══✪〘 MENU 〙✪══
 ║
-╠🍭 *${prefix}figu*
-╠🍭 *${prefix}toimg*
-╠🍭 *${prefix}meme*
-╠🍭 *${prefix}memeindo*
-╠🍭 *${prefix}tts*
-╠🍭 *${prefix}loli [off]*
-╠🍭 *${prefix}nsfwloli [off]*
-╠🍭 *${prefix}url2img*
-╠🍭 *${prefix}leens [na legenda]*
-╠🍭 *${prefix}wait [na legenda]*
-╠🍭 *${prefix}setprefix*
+║🍁 *${prefix}figu*
+║🍁 *${prefix}toimg*
+║🍁 *${prefix}meme*
+║🍁 *${prefix}memeindo*
+║🍁 *${prefix}tts*
+║🍁 *${prefix}loli [off]*
+║🍁 *${prefix}nsfwloli [off]*
+║🍁 *${prefix}url2img*
+║🍁 *${prefix}leens [na legenda]*
+║🍁 *${prefix}wait [na legenda]*
+║🍁 *${prefix}setprefix*
 ║
 ╠══✪〘 IMAGENS 〙✪══
 ║
-╠🍭 *${prefix}loli* [off]
-╠🍭 *${prefix}loli1*
-╠🍭 *${prefix}hentai*
-╠🍭 *${prefix}dono*
-╠🍭 *${prefix}porno*
-╠🍭 *${prefix}boanoite*
-╠🍭 *${prefix}bomdia*
-╠🍭 *${prefix}boatarde*
-╠🍭 *${prefix}mia*
-╠🍭 *${prefix}mia1*
-╠🍭 *${prefix}mia2*
-╠🍭 *${prefix}belle*
-╠🍭 *${prefix}belle1*
-╠🍭 *${prefix}belle2*
-╠🍭 *${prefix}belle3*
-╠🍭 *${prefix}akeno*
-╠🍭 *${prefix}meme*   
-╠🍭 *${prefix}lofi*
-╠🍭 *${prefix}malkova*
-╠🍭 *${prefix}canal*
-╠🍭 *${prefix}nsfwloli1*
-╠🍭 *${prefix}reislin*
+║🍁 *${prefix}loli* [off]
+║🍁 *${prefix}loli1*
+║🍁 *${prefix}hentai*
+║🍁 *${prefix}dono*
+║🍁 *${prefix}porno*
+║🍁 *${prefix}boanoite*
+║🍁 *${prefix}bomdia*
+║🍁 *${prefix}boatarde*
+║🍁 *${prefix}mia*
+║🍁 *${prefix}mia1*
+║🍁 *${prefix}mia2*
+║🍁 *${prefix}belle*
+║🍁 *${prefix}belle1*
+║🍁 *${prefix}belle2*
+║🍁 *${prefix}belle3*
+║🍁 *${prefix}akeno*
+║🍁 *${prefix}meme*   
+║🍁 *${prefix}lofi*
+║🍁 *${prefix}malkova*
+║🍁 *${prefix}canal*
+║🍁 *${prefix}nsfwloli1*
+║🍁 *${prefix}reislin*
 ║
 ╠══✪〘 INTELIGÊNCIA IA 〙✪══
 ║
-╠🍭 *${prefix}simih 1 (para ativar)*
-╠🍭 *${prefix}simih 0 (para desativar)*
-╠🍭 *${prefix}simi (sua mensagem)*
+║🍁 *${prefix}simih 1 (para ativar)*
+║🍁 *${prefix}simih 0 (para desativar)*
+║🍁 *${prefix}simi (sua mensagem)*
 ║
 ╠══✪〘 *RIAS GREMORY* 〙✪══`
 }
