a7fbb3200cba961374de539d6a943b96417a74fd
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-05-20 15:53:26 -0300

    Update menu2.js

--- src/menu2.js
+++ src/menu2.js
@@ -1,49 +1,49 @@
 const menu2 = (prefix) => { 
 	return `                 
-╠══✪〘 menuzinho da akeno 〙✪══
+╠══✪〘 menuzinho da Rias 〙✪══
 ║
-║🍁 *${prefix}figu*
-║🍁 *${prefix}toimg*
-║🍁 *${prefix}meme*
-║🍁 *${prefix}memeindo*
-║🍁 *${prefix}tts*
-║🍁 *${prefix}loli [off]*
-║🍁 *${prefix}nsfwloli [off]*
-║🍁 *${prefix}url2img*
-║🍁 *${prefix}leens [na legenda]*
-║🍁 *${prefix}wait [na legenda]*
-║🍁 *${prefix}setprefix*
+║🌸 *${prefix}figu*
+║🌸 *${prefix}toimg*
+║🌸 *${prefix}meme*
+║🌸 *${prefix}memeindo*
+║🌸 *${prefix}tts*
+║🌸 *${prefix}loli [off]*
+║🌸 *${prefix}nsfwloli [off]*
+║🌸 *${prefix}url2img*
+║🌸 *${prefix}leens [na legenda]*
+║🌸 *${prefix}wait [na legenda]*
+║🌸 *${prefix}setprefix*
 ║
 ╠══✪〘 IMAGENS 〙✪══
 ║
-║🍁 *${prefix}loli* [off]
-║🍁 *${prefix}loli1*
-║🍁 *${prefix}hentai*
-║🍁 *${prefix}porno*
-║🍁 *${prefix}boanoite*
-║🍁 *${prefix}bomdia*
-║🍁 *${prefix}boatarde*
-║🍁 *${prefix}mia*
-║🍁 *${prefix}mia1*
-║🍁 *${prefix}mia2*
-║🍁 *${prefix}belle*
-║🍁 *${prefix}belle1*
-║🍁 *${prefix}belle2*
-║🍁 *${prefix}belle3*
-║🍁 *${prefix}akeno*
-║🍁 *${prefix}meme*   
-║🍁 *${prefix}lofi*
-║🍁 *${prefix}malkova*
-║🍁 *${prefix}play(name music*
-║🍁 *${prefix}nsfwloli1*
-║🍁 *${prefix}reislin*
+║🌸 *${prefix}loli* [off]
+║🌸 *${prefix}loli1*
+║🌸 *${prefix}hentai*
+║🌸 *${prefix}porno*
+║🌸 *${prefix}boanoite*
+║🌸 *${prefix}bomdia*
+║🌸 *${prefix}boatarde*
+║🌸 *${prefix}mia*
+║🌸 *${prefix}mia1*
+║🌸 *${prefix}mia2*
+║🌸 *${prefix}belle*
+║🌸 *${prefix}belle1*
+║🌸 *${prefix}belle2*
+║🌸 *${prefix}belle3*
+║🌸 *${prefix}akeno*
+║🌸 *${prefix}meme*   
+║🌸 *${prefix}lofi*
+║🌸 *${prefix}malkova*
+║🌸 *${prefix}play(name music*
+║🌸 *${prefix}nsfwloli1*
+║🌸 *${prefix}reislin*
 ║
 ╠══✪〘 INTELIGÊNCIA IA 〙✪══
 ║
-║🍁 *${prefix}simih 1 (para ativar)*
-║🍁 *${prefix}simih 0 (para desativar)*
-║🍁 *${prefix}simi (sua mensagem)*
+║🌸 *${prefix}simih 1 (para ativar)*
+║🌸 *${prefix}simih 0 (para desativar)*
+║🌸 *${prefix}simi (sua mensagem)*
 ║
-╠══✪〘 *Akeno Himejima* 〙✪══`
+╠══✪〘 *𝐑𝐢𝐚𝐬 𝐆𝐫𝐞𝐦𝐨𝐫𝐲* 〙✪══`
 }
 exports.menu2 = menu2
