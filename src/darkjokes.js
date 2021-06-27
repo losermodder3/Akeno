3be473043b912a045ac96a507d60483eb6c9a959
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-06-27 14:30:30 -0300

    Update darkjokes.js

--- src/darkjokes.js
+++ src/darkjokes.js
@@ -1,12 +1,13 @@
 [
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJtqk_wKfcyv2WCKyl93E5uBmA-KsnfYy4PQ&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-2bNT_-nmeTrXZ63RFsRZfLd1V5fMk3c1Eg&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTp7tbfQJ4s5uKXuHwuQN3pCKpPyiMIfThfA&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbpVOkhy2fNA97IyVmuw_l9WB2_J0_HnK0zg&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx3WayU11IikO4PFugogqq5BbIMHMic0FbKQ&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB0p6rtutyfrg9hzC3XwknQLNf-aMgaXO0Rw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTmNBgklkzfSStBFZB5AEDiN3fMzil7BVBkA&usqp=CAU"},
-{"result": "https://images3.memedroid.com/images/UPLOADED293/5c6539f6ed225.jpeg"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzLNsY1Aj6x48T9dBJ9l4_2ZuPldaikNCjFA&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0NfqLACUOqJciJ7p0xMliK_yT7B6WsyLogQ&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXk36NWOBIso1d2YCt34mCE6da7-QKKuNwQ&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXk36NWOBIso1d2YCt34mCE6da7-QKKuNwQ&usqp=CAU"},
+{"result": "https://i.pinimg.com/236x/e9/d5/fa/e9d5fa2f3448ae46034fdcec6b490a13.jpg"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXk36NWOBIso1d2YCt34mCE6da7-QKKuNwQ&usqp=CAU"},
+{"result": "https://i.pinimg.com/474x/c8/4e/ce/c84ecea40eb254f502bbb27d0d3dd25b.jpg"},
+{"result": "https://i.pinimg.com/474x/81/b1/b3/81b1b307155040d6e401ad885acbea28.jpg"},
+{"result": "https://i.pinimg.com/474x/ae/09/ba/ae09ba838f1cc0326061621418752fbb.jpg"},
+{"result": "https://i.pinimg.com/474x/70/21/71/702171f8bd64cf93af345e80a5d55624.jpg"},
+{"result": "https://i.pinimg.com/474x/25/98/44/259844825f25c3c76e81dd6c7c2ef216.jpg"},
+{"result": "https://i.pinimg.com/474x/d3/c9/0a/d3c90a6116319be93469c7faa04b9cc4.jpg"}'
+{"result": "https://i.pinimg.com/474x/c7/28/53/c72853740d91e08a406b35dc1e3ebdfa.jpg"}'
 ]
