564b49a38ab1d680eb36e6ea435b0b7fc2b4c61f
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-06-27 17:18:37 -0300

    Update Lolizinha.js

--- src/Lolizinha.js
+++ src/Lolizinha.js
@@ -1,13 +1,16 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXk36NWOBIso1d2YCt34mCE6da7-QKKuNwQ&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXk36NWOBIso1d2YCt34mCE6da7-QKKuNwQ&usqp=CAU"},
-{"result": "https://i.pinimg.com/236x/e9/d5/fa/e9d5fa2f3448ae46034fdcec6b490a13.jpg"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSoJoJAMUnI9GdsTTA3aDzTUhaCH0F4ou_0Q&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmXk36NWOBIso1d2YCt34mCE6da7-QKKuNwQ&usqp=CAU"},
-{"result": "https://i.pinimg.com/474x/c8/4e/ce/c84ecea40eb254f502bbb27d0d3dd25b.jpg"},
-{"result": "https://i.pinimg.com/474x/81/b1/b3/81b1b307155040d6e401ad885acbea28.jpg"},
-{"result": "https://i.pinimg.com/474x/ae/09/ba/ae09ba838f1cc0326061621418752fbb.jpg"},
-{"result": "https://i.pinimg.com/474x/70/21/71/702171f8bd64cf93af345e80a5d55624.jpg"},
-{"result": "https://i.pinimg.com/474x/25/98/44/259844825f25c3c76e81dd6c7c2ef216.jpg"},
-{"result": "https://i.pinimg.com/474x/d3/c9/0a/d3c90a6116319be93469c7faa04b9cc4.jpg"}'
-{"result": "https://i.pinimg.com/474x/c7/28/53/c72853740d91e08a406b35dc1e3ebdfa.jpg"}'
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3l_Pe24T6IKQmJKVDXkASJLqsbc69VTe5RQ&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjR6BtJvWoFGMGu9_gsmPwB3aqILATM8loRA&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMYw04FiYaqq2rTT2cL8IaOmMDDCSEWmq37Q&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmsmlWlu3NG505snlYIpfu1d-crewV5DRjUg&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJx3_Q_fovQX1onE1LjBjzp8xE6nizBAPeKQ&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWERjoDzf4mhC5qJ_GtbUJbUJjl1jA6XeXRg&usqp=CAU"}'
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLEFUkXPZQZ0AjfsGP1ICCcfxG0-K1ZmfY6A&usqp=CAU"}'
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyKTT0IEmQaFCD2O3PzKme9vwg9USnkQ24Hw&usqp=CAU"}'
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTq0Juzz9BzDd3WTsBtXcKL8mL-JCzY70DWHA&usqp=CAU"}'
 ]
+
