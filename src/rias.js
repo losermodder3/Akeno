792ff5fc46b16b70e276e606de3edfe58b0449c7
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-06-28 17:31:46 -0300

    Update rias.js

--- src/rias.js
+++ src/rias.js
@@ -1,6 +1,6 @@
 
 [
-{"result": " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSI-Nh-n9Gu7cp1rG8YjJvTN_Ref0sC9IQdw&usqp=CAU     "},
+{"result": " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSI-Nh-n9Gu7cp1rG8YjJvTN_Ref0sC9IQdw&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsom_6Mta5hLw3ZlaOEXqgULZ1bS3djowIEw&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-iGxABCtpY5A6rJIiL1GuxykXSpY5O9nfCQ&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiJHHHA2gKNj35CXqDNXT_UZ9hUyTia9f7fw&usqp=CAU"}
