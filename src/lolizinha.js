13002612b359553dc4833ffc4abc35717f41e9df
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-06-27 21:24:13 -0300

    Update lolizinha.js

--- src/lolizinha.js
+++ src/lolizinha.js
@@ -8,7 +8,15 @@
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMYw04FiYaqq2rTT2cL8IaOmMDDCSEWmq37Q&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmsmlWlu3NG505snlYIpfu1d-crewV5DRjUg&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJx3_Q_fovQX1onE1LjBjzp8xE6nizBAPeKQ&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWERjoDzf4mhC5qJ_GtbUJbUJjl1jA6XeXRg&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWERjoDzf4mhC5qJ_GtbUJbUJjl1jA6XeXRg&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTliVD2kcF_5I752m_h8V6bPXiuD0uRgkDNaw&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR7YD9OdbDLq_0P_qZJv-9S3wZQVlnqhvKhw&usqp=CAU"},
+{"result": "https://i.pinimg.com/474x/f4/0e/cc/f40ecca5b5749d4141e09fa56dc12196.jpg"},
+{"result": "https://i.pinimg.com/474x/7c/e7/f1/7ce7f1a694866b432751d0977fa06dfe.jpg"},
+{"result": "https://i.pinimg.com/474x/1e/d6/3c/1ed63cafddecb0f875d5517a16e889ef.jpg"},
+{"result": "https://i.pinimg.com/474x/cd/b1/e9/cdb1e999c66ea425e108b5fe1fb773ea.jpg"},
+{"result": "https://i.pinimg.com/474x/96/da/6c/96da6c526babdd97ae698255650051a2.jpg"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQYV02ns5f2--Efoyqd-SlX46DHpeRvITRXA&usqp=CAU"}
 ]
 
 
