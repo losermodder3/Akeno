9799306f317d9d7579e750942bd49aae5621b9df
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-06-28 16:36:15 -0300

    Update rias.js

--- src/rias.js
+++ src/rias.js
@@ -1,10 +1,13 @@
 
 [
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP4YP3LPZmvsHqKf0fssMBCYtllxZzWYoYHQ&usqp=CAU"},
+{"result": " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSI-Nh-n9Gu7cp1rG8YjJvTN_Ref0sC9IQdw&usqp=CAU     "},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsom_6Mta5hLw3ZlaOEXqgULZ1bS3djowIEw&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-iGxABCtpY5A6rJIiL1GuxykXSpY5O9nfCQ&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiJHHHA2gKNj35CXqDNXT_UZ9hUyTia9f7fw&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKBQHJ8iUfh7UMGbBmebVVjE83D9zEQ_22oQ&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUjiGlLHpad2AKvCa0THz8zpY20tGoEdfN6A&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgwsio2jy0WHvdT-hrdGQTA5tdN9usTIaQBw&usqp=CAU"}
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKBQHJ8iUfh7UMGbBmebVVjE83D9zEQ_22oQ&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUjiGlLHpad2AKvCa0THz8zpY20tGoEdfN6A&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgwsio2jy0WHvdT-hrdGQTA5tdN9usTIaQBw&usqp=CAU"}
 ]
-
-
-
