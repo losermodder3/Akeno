5a6371231f20911d2a0db0bdf276ddb023ed008f
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-06-29 21:53:37 -0300

    Update and rename daftarvip.js to gremorys.js

---
+++ src/gremorys.js
@@ -0,0 +1,13 @@
+const gremorys = (prefix) => { 
+	return `
+	
+*Ola se você usou esse comando é por que deseja comprar o vip aqui esta a lista*🍭❤
+──────────────────(♤Rias♡)────────────────────────────────
+ *10 dinheiros ou diamantes no fogareu ( 300) = acesso aos comandos vip e premium🍁🌸
+────────────────(♤Rias♡)─────────────────────────────────
+ *15 dinheiros você tera todas as permissões e podera por a Rias em seu grupo🌸
+────────────────(♤Rias♡)──────────────────────────────────
+ * caso tenha duvidas de .owner*
+*GRUPO DO LOSER♠️🚩 :*https://chat.whatsapp.com/IUW4JzfK71eKgkRIUDPfsf    `
+}
+exports.gremorys = gremorys
