935e18bcc221d1565f78bb24d8b2d4c6de72da61
losermodder2 <84252860+losermodder2@users.noreply.github.com>
2021-07-04 12:39:01 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -14,64 +14,44 @@ const help = (prefix) => {
 ╠══♤〘 MENU 〙♤══
 ║
 ╠🌸*${prefix}figu*
-╠🌸 *${prefix}toimg*
-╠🌸 *${prefix}darkjokes (memes aleatórios)*
-╠🌸 *${prefix}memeindo*
-╠🌸 *${prefix}tts*
-╠🌸 *${prefix}wait [na legenda]*
+╠🌸 *${prefix}toimg(faz figu em foto)
+╠🌸 *${prefix}tts*(idioma e seu texto)
 ║
 ╠══☆〘 NOVOS〙☆══
 ║
+╠🌸 *${prefix}Togif(figu animada)*
+╠🌸 *${prefix}Rename(colocar nome na figu)*
 ╠🌸 *${prefix}attp1(seu texto)*
 ╠🌸 *${prefix}attp2(seu texto)*
 ╠🌸 *${prefix}attp3(seu texto)*
 ╠🌸 *${prefix}attp4(seu texto)*
 ╠🌸 *${prefix}lolizinha*
 ╠🌸 *${prefix}pinterest(nome do personagem)*
-╠🌸 *${prefix}Rias*
-╠🌸 *${prefix}wait(legenda foto de anime)
+╠🌸 *${prefix}rias*
+╠🌸 *${prefix}wait(legenda foto)
 ╠══♧〘 OUTROS 〙♧══
 ║
 ╠🌸 *${prefix}linkgp*
 ╠🌸 *${prefix}simih [1/0]*
 ╠🌸 *${prefix}marcar*
-╠🌸 *${prefix}add [@]*
-╠🌸 *${prefix}banir [@]*
 ╠🌸 *${prefix}promover [@]*
 ╠🌸 *${prefix}rebaixar*
 ╠🌸 *${prefix}admins*
-╠🌸 *${prefix}marcar2*
 ╠🌸 *${prefix}bc [texto]*(somente pro dono)
-╠🌸 *${prefix}marcar3*
 ╠🌸 *${prefix}bloqueados*
-╠🌸 *${prefix}bloquear [@]*
-╠🌸 *${prefix}desbloquear [@]*
-╠🌸 *${prefix}limpar*
-╠🌸 *${prefix}bc [ *texto* ]*
 ╠🌸 *${prefix}bemvindo [1/0]*
 ╠🌸 *${prefix}dono*
 ╠🌸 *${prefix}tts [texto]*
 ╠🌸 *${prefix}grupoinfo*
-╠🌸 *${prefix}ytmp4*
 ╠🌸 *${prefix}bomdia*
 ╠🌸 *${prefix}boanoite*
 ╠🌸 *${prefix}marcar*
-╠🌸 *${prefix}play(nome da musica)*
 ╠══☆〘 IMAGENS 〙☆══
 ║
 ╠🌸 *${prefix}hentai*
 ╠🌸 *${prefix}boanoite*
 ╠🌸 *${prefix}bomdia*
 ╠🌸 *${prefix}boatarde*
-╠🌸 *${prefix}mia*
-╠🌸 *${prefix}rize*
-╠🌸 *${prefix}minato*
-╠🌸 *${prefix}boruto*
-╠🌸 *${prefix}hinata*
-╠🌸 *${prefix}sasuke*
-╠🌸 *${prefix}sakura*
-╠🌸 *${prefix}naruto*
-╠🌸 *${prefix}egirl*  
 ╠🌸 *${prefix}nsfwloli1*
 ║
 ╠══♡〘 INTELIGÊNCIA 〙♡══
@@ -81,10 +61,7 @@ const help = (prefix) => {
 ╠🌸 *${prefix}simi (sua mensagem)*
 ║
 ╠══◇〘 em produção〙◇══
-║
-╠🌸 *${prefix}hentai*
-╠🌸 *${prefix}Egirl*
-╠🌸 *${prefix}doguinho*
+╠🌸*${prefix}hentailist*
 ╠══♧〘 PREMIUM 〙♧══
 ║
 ╠🌸 *${prefix}dado*
@@ -99,13 +76,11 @@ const help = (prefix) => {
 ╠🌸 *${prefix}pack*
 ╠🌸 *${prefix}destrava*
 ╠🌸 *${prefix}gpessoa*
-║
+╠🌸*${prefix}banir (@)
 ╠══♤〘 GRUPO 〙♤══
 ║
-╠🌸 *${prefix}banir*
 ╠🌸 *${prefix}leveling [on/off]*
 ╠🌸 *${prefix}level*
-╠🌸 *${prefix}add*
 ╠🌸 *${prefix}promover*
 ╠🌸 *${prefix}rebaixar*
 ╠🌸 *${prefix}admins*
@@ -128,38 +103,21 @@ const help = (prefix) => {
 ║
 ╠══♤〘 MAIS ALGUNS 〙♧══
 ║
-╠🌸 *${prefix}neko*
 ╠🌸 *${prefix}ttp [texto]*
 ╠🌸 *${prefix}testime*
 ╠🌸 *${prefix}tomp3*
-╠🌸 *${prefix}modonsfw [on/off]*
-╠🌸 *${prefix}happymod [jogo/app]*
-╠🌸 *${prefix}ytsearch*
-╠🌸 *${prefix}moddroid [jogo/app]*
 ╠🌸 *${prefix}nomegp*
 ╠🌸 *${prefix}lolizinha*
 ╠🌸 *${prefix}gay1*
 ╠🌸 *${prefix}next*
 ╠🌸 *${prefix}alerta*
 ╠🌸 *${prefix}belle [img aleatórias]*
-╠🌸 *${prefix}pronomeneu [texto]*
-╠🌸 *${prefix}hobby*
-╠🌸 *${prefix}analise*
 ║
-!------- COMANDOS DE VOZ-------
-╠🌸 *${prefix}onich*
-╠🌸 *${prefix}sexo*
-║🌸 *${prefix}ola
 ╠══°~°〘 OUTROS /2 〙¿~¿══
 ║
 ╠🌸 *${prefix}antilink [1/0]*
-╠🌸 *${prefix}brainly [pergunta}*
 ╠🌸 *${prefix}antiracismo [on/off]*
 ║
-╠══◇〘 INTERATIVOS 〙◇══
-║🌸 *amungus*
-╠🌸 *sexo*
-║🌸 *nyampasu*
 ║╠══🍁〘 𝗗𝗢𝗡𝗢 〙🍁══
 ║
 ║ NOME:  ㇱｌｏｓｅｒㇱ
