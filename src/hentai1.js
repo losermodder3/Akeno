5b63d5c8b1176416d6fd5dc2092fba3caf728f00
losermodder3 <84289562+losermodder3@users.noreply.github.com>
2021-07-08 00:05:39 -0300

    Update hentai1.js

--- src/hentai1.js
+++ src/hentai1.js
@@ -1,12 +1,12 @@
 [
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk4PKeuPJ474f28V_jFCxqnSOYU7zL8yeamQ&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6H1Uq7na0T6Y6Rw1Y-lgoF2y1pRy5Kdmfkg&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs0ao96KCKfgeV4OlI4_Kac94FQIlbxeNaLw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPRh90pXolSeE1tcQAGjIKacYbBEHW6mdE8w&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9Yi97uV67ZfPbtCt8uNC34KEww3-hPgfVoA&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs9FPq9ifloFdN79hod2NwqzqlSkPerbx2pw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs9FPq9ifloFdN79hod2NwqzqlSkPerbx2pw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqMrDVUCmIZu1CoiE6KqqQRPzslzKNi1sgzQ&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFfeIYR_0RMFAyNj_zheOosfzaa38N-LQMKw&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8ppJnLOKbrgqfuADZt3NYC7-jCmJeb0tfRg&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc7NyYMdMBljPmPCZjoeTCZPRMRiYlecKBkA&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGMCWk73m38Iq5qUmRiabHyHdeknt0lUtGtg&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMBvmNhQ0mBfwKfqLsUUHFJ7Ujgg-fYGUqYw&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNnsC3MWamhIeKnUBWmfKCwqPPido4tNcTeA&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8Q3QI38G6y3aa9m1eZTJz3FOWrTjIZhMfRw&usqp=CAU"},
+{"result": " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReCWkPY7wjj3P-V_uazpPdfAV8z6f6Kwy4Eg&usqp=CAU"},
+{"result": ""},
+{"result": ""},
+{"result": ""},
+{"result": ""}
 ]
