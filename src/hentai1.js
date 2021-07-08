6365a954333043a87bdf86ede3320f9530685b14
losermodder3 <84289562+losermodder3@users.noreply.github.com>
2021-07-08 00:12:28 -0300

    Update hentai1.js

--- src/hentai1.js
+++ src/hentai1.js
@@ -4,9 +4,8 @@
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMBvmNhQ0mBfwKfqLsUUHFJ7Ujgg-fYGUqYw&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNnsC3MWamhIeKnUBWmfKCwqPPido4tNcTeA&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8Q3QI38G6y3aa9m1eZTJz3FOWrTjIZhMfRw&usqp=CAU"},
-{"result": " https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReCWkPY7wjj3P-V_uazpPdfAV8z6f6Kwy4Eg&usqp=CAU"},
-{"result": ""},
-{"result": ""},
-{"result": ""},
-{"result": ""}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReCWkPY7wjj3P-V_uazpPdfAV8z6f6Kwy4Eg&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMVEAsOmZmQj8GKFoCQM4a9fxfFIj3kjhkAA&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMZ1ldGhhIcZ1SjYWIdi3IfAhG2Ze8aPP5DQ&usqp=CAU"},
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmo15jOihJ2vn_-eMAulsPBu3T4i_F9At3Ww&usqp=CAU"}
 ]
