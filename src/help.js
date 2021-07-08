dc9792f4d3c1bf71728ecc32b9d7050b3ff75cf2
losermodder3 <84289562+losermodder3@users.noreply.github.com>
2021-07-08 01:26:20 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,131 +1,127 @@
 const help = (prefix) => {
 	return `
 ━━━━━リアス・グレモリー━━━━━━━━
-    𝐑𝐈𝐀𝐒 𝐆𝐑𝐄𝐌𝐎𝐑𝐘
+    *AKENO HIMEJIMA*
 ══════════════════════
-  Menuzinho da Riaszinha
+  Menuzinho da Akeno
 ┌─────────🍭────────
 ╠ 🍭INFO🍭
-╠🌸 𝐃𝐎𝐍𝐎:  ㇱｌｏｓｅｒㇱ
-╠🌸 wa.me/+15877730070
-╠🌸 𝐒𝐓𝐀𝐓𝐔𝐒: ON
-║
+╠🍁 𝐃𝐎𝐍𝐎:  ㇱｌｏｓｅｒㇱ
+╠🍁 𝐒𝐓𝐀𝐓𝐔𝐒: ON
 ║
 ╠══♤〘 MENU 〙♤══
 ║
-╠🌸 *${prefix}figu*
-╠🌸 *${prefix}toimg(faz figu em foto)
-╠🌸 *${prefix}tts*(idioma e seu texto)
+╠🍁 *${prefix}figu*
+╠🍁 *${prefix}toimg(faz figu em foto)
+╠🍁 *${prefix}tts*(idioma e seu texto)
 ║
 ╠══☆〘 NOVOS〙☆══
-╠🌸 *${prefix}ownergroup(dono do grupo num)
-╠🌸 *${prefix}nekoanime*
-╠🌸 *${prefix}
-╠🌸 *${prefix}info*
-╠🌸 *${prefix}semoji*
-╠🌸 *${prefix}Togif(figu animada)*
-╠🌸 *${prefix}Rename(colocar nome na figu)*
-╠🌸 *${prefix}attp1(seu texto)*
-╠🌸 *${prefix}attp2(seu texto)*
-╠🌸 *${prefix}attp3(seu texto)*
-╠🌸 *${prefix}attp4(seu texto)*
-╠🌸 *${prefix}lolizinha*
-╠🌸 *${prefix}pinterest(nome do personagem)*
-╠🌸 *${prefix}rias*
-╠🌸 *${prefix}wait(legenda foto)
+║
+╠🍁 *${prefix}nekoanime*
+╠🍁 *${prefix}hentai1*
+╠🍁 *${prefix}
+╠🍁 *${prefix}semoji*
+╠🍁 *${prefix}Togif(figu animada)*
+╠🍁 *${prefix}Rename(colocar nome na figu)*
+╠🍁 *${prefix}attp1(seu texto)*
+╠🍁 *${prefix}attp2(seu texto)*
+╠🍁 *${prefix}attp3(seu texto)*
+╠🍁 *${prefix}attp4(seu texto)*
+╠🍁 *${prefix}lolizinha*
+╠🍁 *${prefix}pinterest(nome do personagem)*
+╠🍁 *${prefix}wait(legenda foto)
+║
 ╠══♧〘 OUTROS 〙♧══
 ║
-╠🌸 *${prefix}linkgp*
-╠🌸 *${prefix}simih [1/0]*
-╠🌸 *${prefix}marcar*
-╠🌸 *${prefix}promover [@]*
-╠🌸 *${prefix}rebaixar*
-╠🌸 *${prefix}admins*
-╠🌸 *${prefix}bc [texto]*(somente pro dono)
-╠🌸 *${prefix}bloqueados*
-╠🌸 *${prefix}bemvindo [1/0]*
-╠🌸 *${prefix}dono*
-╠🌸 *${prefix}tts [texto]*
-╠🌸 *${prefix}grupoinfo*
-╠🌸 *${prefix}bomdia*
-╠🌸 *${prefix}boanoite*
-╠🌸 *${prefix}marcar*
+╠🍁 *${prefix}linkgp*
+╠🍁 *${prefix}simih [1/0]*
+╠🍁 *${prefix}marcar*
+╠🍁 *${prefix}promover [@]*
+╠🍁 *${prefix}rebaixar*
+╠🍁 *${prefix}admins*
+╠🍁 *${prefix}bc [texto]*(somente pro dono)
+╠🍁 *${prefix}bloqueados*
+╠🍁 *${prefix}bemvindo [1/0]*
+╠🍁 *${prefix}dono*
+╠🍁 *${prefix}tts [texto]*
+╠🍁 *${prefix}grupoinfo*
+╠🍁 *${prefix}bomdia*
+╠🍁 *${prefix}boanoite*
+╠🍁 *${prefix}marcar*
 ╠══☆〘 IMAGENS 〙☆══
 ║
-╠🌸 *${prefix}hentai*
-╠🌸 *${prefix}boanoite*
-╠🌸 *${prefix}bomdia*
-╠🌸 *${prefix}boatarde*
-╠🌸 *${prefix}nsfwloli1*
+╠🍁 *${prefix}hentai*
+╠🍁 *${prefix}boanoite*
+╠🍁 *${prefix}bomdia*
+╠🍁 *${prefix}boatarde*
+╠🍁 *${prefix}nsfwloli1*
 ║
 ╠══♡〘 INTELIGÊNCIA 〙♡══
 ║
-╠🌸 *${prefix}simih 1 (para ativar)*
-╠🌸 *${prefix}simih 0 (para desativar)*
-╠🌸 *${prefix}simi (sua mensagem)*
+╠🍁 *${prefix}simih 1 (para ativar)*
+╠🍁 *${prefix}simih 0 (para desativar)*
+╠🍁 *${prefix}simi (sua mensagem)*
 ║
-╠══◇〘 em produção〙◇══
-╠🌸*${prefix}hentailist*
 ╠══♧〘 PREMIUM 〙♧══
 ║
-╠🌸 *${prefix}dado*
-╠🌸 *${prefix}cekvip*
-╠🌸 *${prefix}premiumlist*
-╠🌸 *${prefix}delete*
-╠🌸 *${prefix}modapk*
-╠🌸 *${prefix}indo10*
-╠🌸 *${prefix}chentai*
-╠🌸 *${prefix}gcpf*
-╠🌸 *${prefix}gbin*
-╠🌸 *${prefix}pack*
-╠🌸 *${prefix}destrava*
-╠🌸 *${prefix}gpessoa*
-╠🌸*${prefix}banir (@)
+╠🍁 *${prefix}dado*
+╠🍁 *${prefix}cekvip*
+╠🍁 *${prefix}premiumlist*
+╠🍁 *${prefix}delete*
+╠🍁 *${prefix}modapk*
+╠🍁 *${prefix}indo10*
+╠🍁 *${prefix}chentai*
+╠🍁 *${prefix}gcpf*
+╠🍁 *${prefix}gbin*
+╠🍁 *${prefix}pack*
+╠🍁 *${prefix}destrava*
+╠🍁 *${prefix}gpessoa*
+╠🍁*${prefix}banir (@)
+║
 ╠══♤〘 GRUPO 〙♤══
 ║
-╠🌸 *${prefix}leveling [on/off]*
-╠🌸 *${prefix}level*
-╠🌸 *${prefix}promover*
-╠🌸 *${prefix}rebaixar*
-╠🌸 *${prefix}admins*
-╠🌸 *${prefix}marcar*
-╠🌸 *${prefix}bemvindo [1/0]*
-╠🌸 *${prefix}grupoinfo*
-╠🌸 *${prefix}bomdia*
-╠🌸 *${prefix}boatarde*
-╠🌸 *${prefix}boanoite*
-╠🌸 *${prefix}setdesc*
+╠🍁 *${prefix}leveling [on/off]*
+╠🍁 *${prefix}level*
+╠🍁 *${prefix}promover*
+╠🍁 *${prefix}rebaixar*
+╠🍁 *${prefix}admins*
+╠🍁 *${prefix}marcar*
+╠🍁 *${prefix}bemvindo [1/0]*
+╠🍁 *${prefix}grupoinfo*
+╠🍁 *${prefix}bomdia*
+╠🍁 *${prefix}boatarde*
+╠🍁 *${prefix}boanoite*
+╠🍁 *${prefix}setdesc*
 ║
 ╠══●〘 ESPECIFICO DO BOT 〙●══
 ║
-╠🌸 *${prefix}bug [sua mensagem]*
-╠🌸 *${prefix}dono*
-╠🌸 *${prefix}gay [@]*
-╠🌸 *${prefix}wame*
-╠🌸 *${prefix}desligar (so para o dono)*
-╠🌸 *${prefix}timer*
+╠🍁 *${prefix}bug [sua mensagem]*
+╠🍁 *${prefix}dono*
+╠🍁 *${prefix}gay [@]*
+╠🍁 *${prefix}wame*
+╠🍁 *${prefix}desligar (so para o dono)*
+╠🍁 *${prefix}timer*
 ║
 ╠══♤〘 MAIS ALGUNS 〙♧══
 ║
-╠🌸 *${prefix}ttp [texto]*
-╠🌸 *${prefix}testime*
-╠🌸 *${prefix}tomp3*
-╠🌸 *${prefix}nomegp*
-╠🌸 *${prefix}lolizinha*
-╠🌸 *${prefix}gay1*
-╠🌸 *${prefix}next*
-╠🌸 *${prefix}alerta*
-╠🌸 *${prefix}belle [img aleatórias]*
+╠🍁 *${prefix}ttp [texto]*
+╠🍁 *${prefix}testime*
+╠🍁 *${prefix}tomp3*
+╠🍁 *${prefix}nomegp*
+╠🍁 *${prefix}gay1*
+╠🍁 *${prefix}next*
+╠🍁 *${prefix}alerta*
+╠🍁 *${prefix}belle [img aleatórias]*
 ║
 ╠══°~°〘 OUTROS /2 〙¿~¿══
 ║
-╠🌸 *${prefix}antilink [1/0]*
-╠🌸 *${prefix}antiracismo [on/off]*
+╠🍁 *${prefix}antilink [1/0]*
+╠🍁 *${prefix}antiracismo [on/off]*
 ║
 ║╠══🍁〘 𝗗𝗢𝗡𝗢 〙🍁══
 ║
-║ NOME:  ㇱｌｏｓｅｒㇱ
-║ INSTA: loser_gremory
+║ NOME: ㇱｌｏｓｅｒㇱ
+║ INSTA: loserzinn
 ║ YOUTUBE: https://youtube.com/channel/UCOjRuKF7S9_y188FQ_F5CbQ
 ║
 ║
