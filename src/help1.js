be62c33b1395d0e08b4606a334de2843fad607a7
losermodder3 <84289562+losermodder3@users.noreply.github.com>
2021-07-08 01:45:34 -0300

    Update help1.js

--- src/help1.js
+++ src/help1.js
@@ -1,32 +1,140 @@
 const help1 = (prefix) => {
-
-	return `
+	
+      return `
 ┣━━━━°❀ ❬ Magias da akeno* ❭ ❀°━━━━┓
 ┃
-┣⊱❥ *xd*
-┣⊱❥ *pinterest*
-┣⊱❥ *anjing*
-┣⊱❥ *loli2*
-┣⊱❥ *anime*
-┣⊱❥ *wolflogo2 <texto | texto>*
-┣⊱❥ *quotemaker <tx | wtrmk | tema>*
-┣⊱❥ *galaxtext*
-┣⊱❥ *textdark*
-┣⊱❥ *textblue*
-┣⊱❥ *lovemake*
-┣⊱❥ *stiltext*
-┣⊱❥ *ninjalogo*
-┣⊱❥ *party*
-┣⊱❥ *rtext*
-┣⊱❥ *water*
-┣⊱❥ *lionlogo <texto | texto>*
-┣⊱❥ *textscreen*
-┣⊱❥ *text3d*
-┣⊱❥ *epep*
-┣⊱❥ *marvelogo <texto | texto>*
-┣⊱❥ *snow <texto | texto>*
-┣⊱❥ *firetext*
+┣━A	
+┣Adventure Kid
+┣━Akiba Girls
+┣━Aki Sora[1]
+┣━Alien from the Darkness
+┣━Angel
+┣━Angel Blade[2]
+┣━Angels in the Court
+Angel of Darkness
+Another Lady Innocent
+B	
+Beat Angel Escalayer
+Behind Closed Doors
+Bible Black
+Blood Royale
+Boku no Pico (Yaoi)
+Bondage Mansion
+Barabanba
+C	
+Campus
+Call Me Tonight
+Cream Lemon
+Crimson Climax[2]
+Cool Devices
+D	
+Daiakuji
+Dark Shell
+Demon Beast Invasion
+Discipline series
+Doctor Shameless
+Dragon Knight series
+Dragon Pink
+E	
+Elven Bride
+Euphoria
+Enzai: Falsely Accused (Yaoi)
+F	
+F³ (Frantic, Frustrated, Female)
+Fencer of Minerva
+Fish in the Trap (Yaoi)
+Futari Ecchi (Step Up Love Story)
+Fuzzy Lips[1]
+G	
+G-Taste
+Girl Next Door
+Green Green: Erolutions
+H	
+Harukoi Otome
+Hatsuinu
+Hininden Gausu
+Hot Tails
+I	
+I Dream of Mimi
+Immoral Sisters series
+Imouto Paradise!
+Isaku
+Itadaki! Seieki[1]
+J	
+Jiburiru - The Devil Angel
+K	
+Kama Sutra (inspired by the book, the Kama Sutra)
+Kanojo x Kanojo x Kanojo
+Kite[1]
+L	
+La Blue Girl series
+Legend of Lyon Flare
+Level C (Yaoi)
+Lolita Anime
+Lolita Anime (Nikkatsu)
+M	
+Magic Woman M
+Magical Canan
+Magical Twilight
+Maki-chan to Nau[3]
+Maple Colors
+Marine a Go Go
+Megachu!
+MeiKing
+Mezzo Forte: Director's Cut
+Midnight Panther
+Milk Money
+Mizuiro
+Moonlight Lady
+My Sexual Harassment (Yaoi)
+Mystery of the Necronomicon
+N	
+Night Shift Nurses[2]
+No Money (Okane ga nai, Yaoi)
+Nozoki Ana
+O	
+Office Lingerie
+Ogenki Clinic Adventures
+P	
+Papillon Rose
+Perverted Thomas
+Prism Ark
+Private Psycho Lesson
+R	
+Rei Rei
+S	
+Sensitive Pornograph (Yaoi)
+Sexy Sailor Soldiers
+Sex Taxi
+Sex Warrior Pudding
+Slave Doll
+Slut Girl
+Spaceship Agga Rutter[1]
+Sora no Iro, Mizu no Iro
+Stainless Night
+Stepmother's Sin
+Steal Moon (Yaoi)
+T	
+Teacher's Pet
+The Rapeman
+Tokio Private Police
+Tournament of the Gods
+Tower of Etruria
+Twin Angels series
+U	
+Urotsukidoji
+V	
+Venus 5
+Viper GTS
+Virgin Night
+Vixens
+W	
+Weather Report Girl
+Welcome to Pia Carrot
+Women at Work
+Words Worth
 
+Fiquei com preguiça de deixar bunito
 ════════════════════
 *  𝐀𝐤𝐞𝐧𝐨 𝐡𝐢𝐦𝐞𝐣𝐢𝐦𝐚  * ♠️
 ════════════════════`
