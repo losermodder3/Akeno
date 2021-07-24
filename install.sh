40f8ad31eafe603cad490d310993f2d919f8818a
losermodder3 <84289562+losermodder3@users.noreply.github.com>
2021-07-24 11:52:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ apt-get install tesseract
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
-echo "[*] Botto o go riyō itadaki arigatōgozaimasu \"Akeno\" loserzinn agradece"
+echo "[*] obrigado por utilizar o bot \"Decimbot\"slazinn agradece"
